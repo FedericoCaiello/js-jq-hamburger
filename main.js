17beac0f7e7830cd386d72b23b924c030bde1ef5
Federico <federico.caiello@gmail.com>
2020-01-16 18:24:48 +0100

    tendina che scompare

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-var hamburger = $('fa-bars');
-
-hamburger.click(
-  function (){
-    $('i').show();
+$('.header-right a').click(
+  function () {
+    $('.hamburger-menu').show(2000)
   }
-)
+);
+
+$('.close').click(
+  function() {
+    $('.hamburger-menu').hide()
+  }
+);
